bbb96d57f4a86a3ecfd5ebc9aa19206d9b18a780
Thomas Rowlands <thomasrowlands@users.noreply.github.com>
2018-12-18 13:59:53 -0500

    Begin redesign

--- gulpfile.js
+++ gulpfile.js
@@ -131,4 +131,5 @@ gulp.task('dev', ['css', 'js', 'browserSync'], function() {
   gulp.watch('./scss/*.scss', ['css']);
   gulp.watch('./js/*.js', ['js']);
   gulp.watch('./*.html', browserSync.reload);
+  gulp.watch('./pages/*.html', browserSync.reload)
 });
